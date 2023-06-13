83ff3ad8af6cdb7518b898b00f5e06ebd2bef835
delirium-dj <25088798+delirium-dj@users.noreply.github.com>
2023-06-13 11:00:24 +0200

    v0.1.5.3

--- src/components/component1.js
+++ src/components/component1.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
+// import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
 
 export default function Component1() {
   return (
@@ -36,34 +36,32 @@ export default function Component1() {
             </div>
           </div>
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-            <dt className="flex flex-col items-start">
-              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+            <div className="flex flex-col items-start">
+              {/* <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
                 <CalendarDaysIcon
                   className="h-6 w-6 text-white"
                   aria-hidden="true"
                 />
-              </div>
-              <div className="mt-4 font-semibold text-white">
-                Weekly articles
-              </div>
+              </div> */}
+              <dt className="mt-4 font-semibold text-white">Weekly articles</dt>
               <dd className="mt-2 leading-7 text-gray-400">
                 Non laboris consequat cupidatat laborum magna. Eiusmod non irure
                 cupidatat duis commodo amet.
               </dd>
-            </dt>
-            <dt className="flex flex-col items-start">
-              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+            </div>
+            <div className="flex flex-col items-start">
+              {/* <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
                 <HandRaisedIcon
                   className="h-6 w-6 text-white"
                   aria-hidden="true"
                 />
-              </div>
-              <div className="mt-4 font-semibold text-white">No spam</div>
+              </div> */}
+              <dt className="mt-4 font-semibold text-white">No spam</dt>
               <dd className="mt-2 leading-7 text-gray-400">
                 Officia excepteur ullamco ut sint duis proident non adipisicing.
                 Voluptate incididunt anim.
               </dd>
-            </dt>
+            </div>
           </dl>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Component1 from "../components/component1";
 import Component2 from "../components/component2";
-import Component3 from "../components/component3";
+// import Component3 from "../components/component3"; (can't solve the problem with layout shifts when using Titillium Web font)
 // import Component4 from "../components/component4";
 import Component5 from "../components/component5";
 import Component6 from "../components/component6";
@@ -17,7 +17,6 @@ export default function Index() {
       <Component5 />
       <Component1 />
       <Component2 />
-      <Component3 />
       <Component6 />
       <Footer />
     </main>
@@ -28,7 +27,7 @@ export function Head() {
   return (
     <>
       <html lang="en" />
-      <title>v0.1.5 Gatsby/Tailwind Product Template</title>
+      <title>v0.1.5.3 Gatsby/Tailwind Product Template</title>
     </>
   );
 }
